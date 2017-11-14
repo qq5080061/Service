512c30d79870e76057827310de2b0a282a69ae20
paas <paas@cse-codegen-backend-252369159-n8vmc>
2017-11-14 14:55:01 +0000

    Auto pushed by ServiceStage!

---
+++ src/main/java/com/service/controller/ServiceImpl.java
@@ -0,0 +1,65 @@
+package com.service.controller;
+
+import com.service.model.Person;
+
+import javax.ws.rs.core.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import io.servicecomb.provider.rest.common.RestSchema;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.CseSpringCodegen", date = "2017-11-14T14:55:01.239Z")
+
+@RestSchema(schemaId = "service")
+@RequestMapping(path = "/service", produces = MediaType.APPLICATION_JSON)
+public class ServiceImpl {
+
+    @Autowired
+    private ServiceDelegate userServiceDelegate;
+
+
+    @RequestMapping(value = "/add",
+        produces = { "application/json" }, 
+        method = RequestMethod.GET)
+    public Integer add( @RequestParam(value = "a", required = true) Integer a, @RequestParam(value = "b", required = true) Integer b){
+
+        return userServiceDelegate.add(a, b);
+    }
+
+
+    @RequestMapping(value = "/sayhei",
+        produces = { "application/json" }, 
+        method = RequestMethod.GET)
+    public String sayHei( @RequestHeader(value="name", required=true) String name){
+
+        return userServiceDelegate.sayHei(name);
+    }
+
+
+    @RequestMapping(value = "/sayhello/{name}",
+        produces = { "application/json" }, 
+        method = RequestMethod.POST)
+    public String sayHello( @PathVariable("name") String name){
+
+        return userServiceDelegate.sayHello(name);
+    }
+
+
+    @RequestMapping(value = "/sayhi",
+        produces = { "application/json" }, 
+        method = RequestMethod.GET)
+    public String sayHi( @RequestParam(value = "name", required = true) String name){
+
+        return userServiceDelegate.sayHi(name);
+    }
+
+
+    @RequestMapping(value = "/saysomething",
+        produces = { "application/json" }, 
+        method = RequestMethod.POST)
+    public String saySomething( @RequestParam(value = "prefix", required = true) String prefix, @RequestBody Person user){
+
+        return userServiceDelegate.saySomething(prefix, user);
+    }
+
+}
